be095527df5a8e0c38c9eb8c7d536d1dce561995
jayacerlaptop <jayacerlaptop@gmail.com>
2018-05-06 02:39:48 -0400

    hahah

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-employee',
+    template: ` <table>
+    <tr>
+        <td>First Name</td>
+        <td>{{firstName}}</td>
+    </tr>
+    <tr>
+        <td>Last Name</td>
+        <td>{{lastName}}</td>
+    </tr>
+    <tr>
+        <td>Gender</td>
+        <td>{{gender}}</td>
+    </tr>
+    <tr>
+        <td>Ag</td>
+        <td>{{age}}</td>
+    </tr>
+</table> `
+})
+//*
+export class EmployeeComponent {
+    firstName: string = 'Tffhhhhhgggggffomvy';
+    lastName: string = 'Hopyykcccins';
+    gender: string = 'Male';
+    age: number = 20;
+}
